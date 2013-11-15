9b9cce354213f77cdc014036b7bf3d0359767e0e
Tianlong Wu <rockuw@gmail.com>
2013-11-15 14:02:37 +0800

    add c++ indent style

--- init-c-mode.el
+++ init-c-mode.el
@@ -9,6 +9,21 @@
   (gtags-mode t))
 
 (add-hook 'c-mode-common-hook 'my-c-mode-hook)
+
+(defun my-c++-mode-hook ()
+  (c-set-style "linux")
+  (setq c-basic-offset 4)
+  (setq tab-width 4)
+  (c-set-offset 'innamespace '0)
+  (c-set-offset 'inextern-lang '0)
+  (c-set-offset 'inline-open '0)
+  (c-set-offset 'label '*)
+  (c-set-offset 'case-label '*)
+  (c-set-offset 'access-label '/)
+  (setq indent-tabs-mode nil))
+
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
 (provide 'init-c-mode)
