817370932f37ddfbc6497b917bbc1882b78443af
Tianlong Wu <rockuw@gmail.com>
2014-01-23 16:55:48 +0800

    set tab with to 4 in go mode

--- init-go-mode.el
+++ init-go-mode.el
@@ -1,8 +1,13 @@
 ;;; golang mode
 
+(require 'go-mode-load)
+
 (setq load-path
       (cons "/usr/local/Cellar/go/1.1//misc/emacs/" load-path))
 
-(require 'go-mode-load)
+(defun my-go-mode-hook ()
+  (setq tab-width 4))
+
+(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (provide 'init-go-mode)
